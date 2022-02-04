5afbd3b790391271d8c91b7dbcd3dccfd8eeeb15
Jhousua <jhousua.jumalon46@gmail.com>
2022-02-04 23:37:05 +0800

    NSTP_SYSTEM
    set

--- NSTP/NstpSystem/settings.py
+++ NSTP/NstpSystem/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'nstpsystem',
-        'USER': 'admin',
-        'PASSWORD': '',
+        'USER': 'root',
+        # 'PASSWORD': '',
         'HOST': 'localhost',
         'OPTIONS':{
             'sql_mode': 'traditional',
